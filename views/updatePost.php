45c02eac5c5d66ead5872b15f690eb0176148e6d
Breno Barbosa <breno.barbosa@auditto.online>
2024-09-23 11:25:54 -0300

    update

--- views/updatePost.php
+++ views/updatePost.php
@@ -1,20 +1,25 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Editar Post</title>
+    <title>Atualizar Post</title>
+    <link rel="stylesheet" href="<?= 'views/styles/updatePost.css'?>">
 </head>
 <body>
-    <h1>Editar Post</h1>
-    <form method="post" action="/bloguitto/update">
-        <input type="hidden" name="post_id" value="<?php echo ($post->post_id); ?>">
+    <div class="container">
+        <h1>Atualizar Post</h1>
+        <form method="post" action="/bloguitto/update">
+            <input type="hidden" name="post_id" value="<?php echo ($post->post_id); ?>">
 
-        <label for="title">Título:</label>
-        <input type="text" id="title" name="title" value="<?php echo ($post->title); ?>" required><br>
+            <label for="title">Título:</label>
+            <input type="text" id="title" name="title" value="<?php echo ($post->title); ?>" required><br>
 
-        <label for="description">Descrição:</label>
-        <textarea id="description" name="description" required><?php echo ($post->description); ?></textarea><br>
+            <label for="description">Descrição:</label>
+            <textarea id="description" name="description" required><?php echo ($post->description); ?></textarea><br>
 
-        <button type="submit">Atualizar Post</button>
-    </form>
+            <button type="submit">Atualizar Post</button>
+          
+        </form>
+    </div>
+    
 </body>
 </html>
